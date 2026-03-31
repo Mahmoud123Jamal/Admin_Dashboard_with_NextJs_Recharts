f7eb742adfd1045d9edb18a80bdadc907d258bcd
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-03-31 17:29:56 +0200

    feat:  update home page dashboard and add overview page in

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import SideBar from "@/components/SideBar";
+import Overview from "./overview/page";
 
 export default function Home() {
   return (
     <div>
-      <SideBar />
+      <Overview />
     </div>
   );
 }

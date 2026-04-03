3b478114d2d34a913ee3da25c93c8a127d71e1c1
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-04-03 04:30:44 +0200

    feat: update products page with summary statistics cards(Update values)

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -13,13 +13,13 @@ function ProductsPage() {
     <div className="flex-1 relative z-10 overflow-auto">
       <main className="max-w-7xl mx-auto p-4 py-8 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4">
-          <StateCard name="Total Products" icon={ShoppingBag} value="158.22$" />
-          <StateCard name="Total Stock" icon={SquareActivity} value="158,888" />
-          <StateCard name="Total Sold" icon={DollarSign} value="758" />
+          <StateCard name="Total Products" icon={ShoppingBag} value="4,444" />
+          <StateCard name="Total Stock" icon={SquareActivity} value="15,888" />
+          <StateCard name="Total Sold" icon={DollarSign} value="15084.22$" />
           <StateCard
             name="Total Categories"
             icon={ChartBarStacked}
-            value="58,800"
+            value="10"
           />
         </div>
       </main>

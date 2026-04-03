442de547e0c13b2664a14a963a21fec2ac464996
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2026-04-03 04:26:58 +0200

    feat: update products page with summary statistics cards

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,30 @@
+"use client";
+
+import StateCard from "@/components/StateCard";
+import {
+  ChartBarStacked,
+  DollarSign,
+  ShoppingBag,
+  SquareActivity,
+} from "lucide-react";
+
 function ProductsPage() {
-  return <div>ProductsPage</div>;
+  return (
+    <div className="flex-1 relative z-10 overflow-auto">
+      <main className="max-w-7xl mx-auto p-4 py-8 sm:px-6 lg:px-8">
+        <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4">
+          <StateCard name="Total Products" icon={ShoppingBag} value="158.22$" />
+          <StateCard name="Total Stock" icon={SquareActivity} value="158,888" />
+          <StateCard name="Total Sold" icon={DollarSign} value="758" />
+          <StateCard
+            name="Total Categories"
+            icon={ChartBarStacked}
+            value="58,800"
+          />
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default ProductsPage;
